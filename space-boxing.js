e8336dbef8d013624c4f2c2ef3cfcc71f11d8aaa
Barbue <david.crochet@codeimmersives.com>
2022-09-25 17:36:05 -0500

    update space-boxing file

--- space-boxing.js
+++ space-boxing.js
@@ -5,11 +5,11 @@ let earthweight = Number(prompt("Enter your Earth weight: "));console.log(earthw
 let planetNum = Number(prompt("Please enter the number of the planet you will be traveling to using the following system. 1 - Venus, 2-Mars, 3-Jupiter, 4-Saturn, 5-Uranus, 6-Neptune:  "));
 console.log(planetNum);
 
-if(planetNum === 1){console.log(128*0.78)}
-else if(planetNum === 2){console.log(128*0.39)}
-else if(planetNum === 3){console.log(128*2.65)}
-else if(planetNum === 4){console.log(128*1.17)}
-else if(planetNum === 5){console.log(128*1.05)}
-else if(planetNum === 6){console.log(128*1.23)}
+if(planetNum === 1){console.log(earthweight*0.78)}
+else if(planetNum === 2){console.log(earthweight*0.39)}
+else if(planetNum === 3){console.log(earthweight*2.65)}
+else if(planetNum === 4){console.log(earthweight*1.17)}
+else if(planetNum === 5){console.log(earthweight*1.05)}
+else if(planetNum === 6){console.log(earthweight*1.23)}
 
 
